6c9ac774e06da4c2366da60275603bccb1da8859
honor0425 <honor0425@gmail.com>
2025-05-05 19:25:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const OpenAI = require("openai");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
